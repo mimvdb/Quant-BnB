644fafb6607953817d4ade38ec4fe72350fbb020
Mim van den Bos <mimvandenbos@gmail.com>
2025-05-07 02:05:56 +0200

    Add depth 3 calls

--- call.jl
+++ call.jl
@@ -13,3 +13,17 @@ function optimal_regression_2d(X, Y)
 
     return (opt, opt_tree)
 end
+
+function optimal_classification_3d(X, Y, timelimit)
+    gre, gre_tree = greedy_tree(X, Y, 3, "C")
+    opt, opt_tree = QuantBnB_3D(X, Y, 3, 3, gre*(1+1e-6), 0, 0, nothing, "C", timelimit)
+
+    return (opt, opt_tree)
+end
+
+function optimal_regression_3d(X, Y, timelimit)
+    gre, gre_tree = greedy_tree(X, Y, 3, "R")
+    opt, opt_tree = QuantBnB_3D(X, Y, 3, 3, gre*(1+1e-6), 0, 0, nothing, "R", timelimit)
+
+    return (opt, opt_tree)
+end
